7ffe489f39c5711ea40afa47f892e3418e25a8c2
Amir Kasumovic <amir@ideaology.io>
2021-06-17 12:57:22 +0200

    Images

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
     // When clicked, show difference
     $('#transparentmap AREA').click(function() {
-      var theDifference = '#' + $(this).attr('id') + '-diff';
-      $(theDifference).css('display', 'inline');
-      $(theDifference).data('clicked', true);
-  
-      // When all differences selected/clicked, show email submission form
-      if ($('#leaf-diff').data('clicked') && $('#petal-diff').data('clicked') && $('#light-diff').data('clicked') && $('#nolight-diff').data('clicked') && $('#sauce-diff').data('clicked')) {
-        // $('#form_container').css('display', 'inline');
-      }
-  
+        var theDifference = '#' + $(this).attr('id') + '-diff';
+        $(theDifference).css('display', 'inline');
+        $(theDifference).data('clicked', true);
+    
+        // When all differences selected/clicked, show email submission form
+        if ($('#leaf-diff').data('clicked') && $('#petal-diff').data('clicked') && $('#light-diff').data('clicked') && $('#nolight-diff').data('clicked') && $('#sauce-diff').data('clicked')) {
+            $('#form_container').css('display', 'inline');
+        }
     });
-  });
-  
+});
