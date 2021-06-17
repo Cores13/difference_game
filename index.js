a4a15d0e3235ec8ab30487acebeabdfeec79c422
Amir Kasumovic <amir@ideaology.io>
2021-06-17 13:46:04 +0200

    Functional

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ $(document).ready(function() {
     
         // When all differences selected/clicked, show email submission form
         if ($('#leaf-diff').data('clicked') && $('#petal-diff').data('clicked') && $('#light-diff').data('clicked') && $('#nolight-diff').data('clicked') && $('#sauce-diff').data('clicked')) {
-            $('#form_container').css('display', 'inline');
+            $('.popup').css('display', 'inline');
+            document.querySelector(".popup").classList.add("active");
         }
     });
+    document.querySelector(".popup .close-btn").addEventListener("click",function(){
+        document.querySelector(".popup").classList.remove("active");
+    });
 });
+
